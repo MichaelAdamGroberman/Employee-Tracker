5dde36fdf0a16d808f049d183338ec2666615556
Michael Adam Groberman <michael.groberman@me.com>
2021-11-05 14:44:35 -0400

    created connection.js
    finished the script to configure the MYSQL Server connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// mySQL package that makes the magic happen
+const mysql = require('mysql2');
+
+// importing and requiring .env authentication file
+require('dotenv').config();
+
+// define mySQL server connection auth
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306;
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'employees'
+});
+
+module.export = connection;
