3a689daf632b46a825484f5429889e766bc6068f
Michael Adam Groberman <michael.groberman@me.com>
2021-11-09 16:28:11 -0500

    updated index.js
    decided to add all my inquirer prompts here instead of defining classes in a lib directory. Added chalk for styling our CLI output, and console.log to display the data in table view. I added a mainMenu inquirer prompt, and if statements to run functions if an option is selected.

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-const connection = require("./config/connection");
-const inquirer = require("inquirer");
-const chalk = require("chalk");
-require("console.table");
+const connection = require('./config/connection');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+require('console.table');
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Successfully connected to MYSQL Server!');
+  console.log(chalk.blueBright.bold('Employee Tracker'));
+  console.log(chalk.blueBright.italic('Developed by Michael Adam Groberman'));
+  main();
+});
+
+const main = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'mainMenu',
+        type: 'list',
+        message: 'Please select an option from the main menu:',
+        choices: [
+          'View All Departments',
+          'View All Roles',
+          'View All Employees',
+          'Add a New Department',
+          'Add a New Role',
+          'Add a New Employee',
+          'Updating Existing Employee',
+          'Quit',
+        ],
+      },
+    ])
+    .then((answer) => {
+      const { choice } = answer;
+      if (choice === 'View All Departments') {
+        viewAllDepartments();
+      }
+      if (choice === 'View All Roles') {
+        viewAllRoles();
+      }
+      if (choice === 'View All Employees') {
+        viewAllEmployees();
+      }
+      if (choice === 'Add a New Department') {
+        addDepartment();
+      }
+      if (choice === 'Add a New Role') {
+        addRole();
+      }
+      if (choice === 'Add a New Employee') {
+        addEmployee();
+      }
+      if (choice === 'Updating Existing Employee') {
+        updateUser();
+      }
+      if (choice === 'Quit') {
+        connection.end();
+      }
+    });
+};
