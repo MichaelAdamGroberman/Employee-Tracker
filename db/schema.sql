e42f9cdf2ddbe28f2a559021b8d9a0ef74cea8c4
Michael Adam Groberman <michael.groberman@me.com>
2021-11-10 11:07:12 -0500

    updated schema
    was having some typo causing an issue, so i just simplified and made everything singular

--- db/schema.sql
+++ db/schema.sql
@@ -8,27 +8,30 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 -- create a table to store the department data
-CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 -- create TABLE for role data; include:[id,title,salary,department_id, FOREIGN KEY]
+DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(8,2),
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
     ON DELETE CASCADE
 );
 -- create table to store employee data; include: [id,first_name,last_name,role_id, manager_id, and a foreign key]
-CREATE TABLE employees (
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employees(id),
-    role_id INT, 
-    FOREIGN KEY (role_id) REFERENCES roles(id)
+    manager_id INT
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    role_id INT
+    FOREIGN KEY (role_id) REFERENCES role(id)
     ON DELETE CASCADE
 );
